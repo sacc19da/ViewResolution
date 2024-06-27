484d03dca3bc7a490c5b4a8f025d5d250a7ca8e8
Sacc_Wu <Sacc19da@gmail.com>
2024-06-27 14:50:41 +0800

    新增裝置區域解析偵測

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ function detectDevice() {
   const innerHeight = window.innerHeight;
   const clientWidth = document.documentElement.clientWidth;
   const clientHeight = document.documentElement.clientHeight;
+  const htmlWidth = document.documentElement.offsetWidth;
+  const htmlHeight = document.documentElement.offsetHeight;
+  const bodyWidth = document.body.offsetWidth;
+  const bodyHeight = document.body.offsetHeight;
   let deviceType = "桌上電腦或筆電";
 
   if (isMobileDevice()) {
@@ -22,6 +26,8 @@ function detectDevice() {
   document.getElementById('device-type').innerText = `裝置類型: ${deviceType}`;
   document.getElementById('innerResolution').innerText = `window.inner解析度: ${innerWidth} x ${innerHeight}`;
   document.getElementById('documentClientResolution').innerText = `document.documentElement.client解析度: ${clientWidth} x ${clientHeight}`;
+  document.getElementById('htmlResolution').innerText = `HTML 框線解析度: ${htmlWidth} x ${htmlHeight}`;
+  document.getElementById('bodyResolution').innerText = `BODY 框線解析度: ${bodyWidth} x ${bodyHeight}`;
 }
 
 window.onload = detectDevice;
